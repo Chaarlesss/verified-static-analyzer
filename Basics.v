faa20dc58ad1fcc8a13ee8b875b99e0f26a36e33
Gogume1er <gogume1er@gmail.com>
2023-03-26 22:04:58 +0200

    WIP...

--- Basics.v
+++ Basics.v
@@ -28,75 +28,8 @@ Notation "(≡)" := eq (only parsing) : vsa.
 Notation "( x ≡)" := (eq x) (only parsing) : vsa.
 Notation "(≡ x )" := (fun y => eq y x) (only parsing) : vsa.
 
-Notation "x ↾ p" := (exist _ x p) (at level 20) : vsa.
-
 Class Setoid (A: Type) `{E: Equiv A}: Prop :=
   setoid_equiv :> Equivalence (=).
-
-Record Sett (A: Type) `{Setoid A}: Type := {
-    set_prop: A -> Prop;
-    set_proper :> Proper ((=) ==> iff) (set_prop)
-}.
-
-Definition SetContains {A: Type} `{Setoid A} (x: A) (P: Sett A): Prop :=
-  set_prop A P x.
-
-#[program]
-Definition SetSingleton {A: Type} `{Setoid A} (x: A): Sett A :=
-  {| set_prop := fun y => y = x |}.
-Next Obligation. solve_proper. Qed.
-
-#[program]
-Definition SetList {A: Type} `{Setoid A} (l: list A): Sett A :=
-  let f := fun t => (fix f l' :=
-        match l' with
-        | nil => False
-        | cons x q => t = x \/ (f q)
-        end) in
-  {| set_prop := fun x => f x l |}.
-Next Obligation.
-  split; induction l; auto; intros [|]; firstorder.
-Qed.
-
-#[program]
-Definition SetEmpty {A: Type} `{Setoid A}: Sett A :=
-  {| set_prop := fun _ => False |}.
-Next Obligation. firstorder. Qed.
-
-#[program]
-Definition SetFull {A: Type} `{Setoid A}: Sett A :=
-  {| set_prop := fun _ => True |}.
-Next Obligation. firstorder. Qed.
-
-Notation "'℘' A" := (Sett A) (at level 0).
-Notation "x ∈ P" := (SetContains x P) (at level 19).
-Notation "{{ x }}" := (SetSingleton x).
-Notation "{{ x ; y ; .. ; z }}" := (SetList (cons x (cons y (.. (cons z nil) ..)))).
-Notation "∅" := SetEmpty.
-
-Lemma set_contains_singleton {A: Type} `{Setoid A} (x: A):
-  forall u, u ∈ {{ x }} <-> u = x.
-Proof. reflexivity. Qed.
-
-Lemma set_contains_empty {A: Type} `{Setoid A}:
-  forall u, u ∈ ∅ -> False.
-Proof. contradiction. Qed.
-
-Lemma set_contains_full {A: Type} `{Setoid A}:
-  forall u, u ∈ SetFull.
-Proof. firstorder. Qed.
-
-Class SgOp A `{Setoid A} := sg_op: A -> A -> A.
-Class SgSetOp A `{Setoid A} := sg_set_op: ℘ A -> A.
-Class MonUnit A `{Setoid A} := mon_unit: A.
-
-#[global]
-Typeclasses Transparent SgOp SgSetOp MonUnit.
-
-Infix "&" := sg_op (at level 50, left associativity) : vsa.
-Notation "(&)" := sg_op (only parsing) : vsa.
-Notation "( x &)" := (sg_op x) (only parsing) : vsa.
-Notation "(& x )" := (fun y => y & x) (only parsing) : vsa.
 
 Class Poset (A: Type) `{E: Equiv A} `{O: Ord A}: Prop := {
   poset_setoid :> Setoid A;
@@ -105,9 +38,6 @@ Class Poset (A: Type) `{E: Equiv A} `{O: Ord A}: Prop := {
   poset_trans :> Transitive (⊑);
   poset_proper :> Proper ((=) ==> (=) ==> iff) (⊑)
 }.
-
-Definition UpperBound {A: Type} `{Poset A} (S: ℘ A) (u: A) := forall x, x ∈ S -> x ⊑ u.
-Definition LowerBound {A: Type} `{Poset A} (S: ℘ A) (u: A) := forall x, x ∈ S -> u ⊑ x.
 
 Section Dual.
 
@@ -147,6 +77,8 @@ Section Dual.
 
 End Dual.
 
+
+
 Section PropSetoid.
 
   #[global]
@@ -163,67 +95,7 @@ Section PropSetoid.
 
 End PropSetoid.
 
-Section Pointwise.
 
-  Context (X A: Type) {E: Equiv A} {O: Ord A}.
-
-  #[global]
-  Instance PointwiseEquiv: Equiv (X -> A) :=
-    fun f g => forall (x: X), f x = g x.
-
-  #[global]
-  Instance PointwiseOrd: Ord (X -> A) :=
-    fun f g => forall (x: X), f x ⊑ g x.
-
-  #[program, global]
-  Instance PointwiseEquiv_Equivalence {St: Setoid A}: Equivalence PointwiseEquiv.
-  Solve All Obligations with firstorder.
-
-  #[global]
-  Instance PointwiseOrd_Reflexive {P: Poset A}: Reflexive PointwiseOrd.
-  Proof. firstorder. Qed.
-
-  #[global]
-  Instance PointwiseOrd_Antisymmetric {P: Poset A}: Antisymmetric (X -> A) PointwiseEquiv PointwiseOrd.
-  Proof.
-    intros f g H1 H2 x. now apply antisymmetry.
-  Qed.
-
-  #[global]
-  Instance PointwiseOrd_Transitive {P: Poset A}: Transitive PointwiseOrd.
-  Proof. firstorder. Qed.
-
-  #[program, global]
-  Instance PointwisePoset {P: Poset A}: Poset (X -> A).
-  Next Obligation.
-    intros f f' H__f g g' H__g. split; intros H x; cbv in H__f, H__g.
-    - rewrite <- H__f. rewrite <- H__g. apply H.
-    - rewrite H__f. rewrite H__g. apply H.
-  Qed.
-
-End Pointwise.
-
-Section Powerset.
-
-  Context {X: Type} `{Setoid X}.
-
-  #[global]
-  Instance PowersetEquiv: Equiv (℘ X) :=
-    fun P Q => forall f, f ∈ P <-> f ∈ Q.
-
-  #[global]
-  Instance PowersetOrd: Ord (℘ X) :=
-    fun P Q => forall x, x ∈ P -> x ∈ Q.
-
-  #[program, global]
-  Instance PowersetSetoid: Setoid (℘ X).
-  Solve All Obligations with firstorder.
-
-  #[program, global]
-  Instance PowersetPoset: Poset (℘ X).
-  Solve All Obligations with firstorder.
-
-End Powerset.
 
 Section Projection.
 
@@ -254,6 +126,10 @@ Section Projection.
 
 End Projection.
 
+
+
+Notation "x ↾ p" := (exist _ x p) (at level 20) : vsa.
+
 Definition SigEquiv {A: Type} `{Equiv A} (P: A -> Prop) : Equiv (sig P) := fun x y => `x = `y.
 Definition SigOrd {A: Type} `{Ord A} (P: A -> Prop) : Ord (sig P) := fun x y => `x ⊑ `y.
 Ltac simpl_sig_setoid :=
@@ -278,3 +154,156 @@ Proof. now apply (projected_setoid (@proj1_sig _ P)). Qed.
 #[global]
 Instance SigPoset {A: Type} `{Poset A} (P : A -> Prop) : Poset (sig P).
 Proof. now apply (projected_poset (@proj1_sig _ P)). Qed.
+
+Structure Morphism (X A: Type) `{!Equiv X} `{!Equiv A} : Type := {
+  morphism : X -> A;
+  morphism_proper :> Proper ((=) ==> (=)) morphism
+}.
+
+Coercion morphism : Morphism >-> Funclass.
+Notation "X → A" := (Morphism X A) (at level 80, right associativity).
+
+Section Pointwise.
+
+  Context (X A: Type) `{!Equiv X} `{!Equiv A} `{!Ord A}.
+
+  #[global]
+  Instance PointwiseEquiv: Equiv (X → A) :=
+    fun f g => forall (x: X), f x = g x.
+
+  #[global]
+  Instance PointwiseOrd: Ord (X → A) :=
+    fun f g => forall (x: X), f x ⊑ g x.
+
+  #[program, global]
+  Instance PointwiseEquiv_Equivalence `{!Setoid A}: Equivalence PointwiseEquiv.
+  Solve All Obligations with firstorder.
+
+  #[global]
+  Instance PointwiseOrd_Reflexive `{!Poset A}: Reflexive PointwiseOrd.
+  Proof. firstorder. Qed.
+
+  #[global]
+  Instance PointwiseOrd_Antisymmetric `{!Poset A}: Antisymmetric (X → A) PointwiseEquiv PointwiseOrd.
+  Proof.
+    intros f g H1 H2 x. now apply antisymmetry.
+  Qed.
+
+  #[global]
+  Instance PointwiseOrd_Transitive `{!Poset A}: Transitive PointwiseOrd.
+  Proof. firstorder. Qed.
+
+  #[program, global]
+  Instance PointwisePoset `{!Poset A}: Poset (X → A).
+  Next Obligation.
+    intros f f' H__f g g' H__g.
+    unfold equiv in H__f, H__g. unfold PointwiseEquiv in H__f, H__g. unfold ord. unfold PointwiseOrd.
+    (* TODO: (=) valid under equiv, impl, forall & exist *)
+    split; intros H x.
+    - rewrite <- H__f. rewrite <- H__g. apply H.
+    - rewrite H__f. rewrite H__g. apply H.
+  Qed.
+
+End Pointwise.
+
+Class Sett (A: Type) `{!Equiv A} : Type := {
+  sett : A -> Prop;
+  set_proper :> Proper ((=) ==> iff) sett
+}.
+
+Coercion sett : Sett >-> Funclass.
+Notation "℘ A" := (Sett A) (at level 79).
+
+Definition SetContains {A: Type} `{!Equiv A} (x: A) (P: ℘ A): Prop :=
+  sett x.
+
+#[program]
+Definition SetSingleton {A: Type} `{Setoid A} (x: A): ℘ A :=
+  Build_Sett _ _ (fun t => t = x) _.
+Next Obligation. solve_proper. Qed.
+#[program]
+Definition SetList {A: Type} `{Setoid A} (l: list A): ℘ A :=
+  Build_Sett _ _ (fun t => (fix f l' :=
+          match l' with
+          | nil => False
+          | cons x q => t = x \/ (f q)
+          end) l) _.
+Next Obligation.
+  split; induction l; auto; intros [|]; [left | right | left | right]; firstorder.
+Qed.
+#[program]
+Definition SetProp {A: Type} `{Setoid A} (P: A -> Prop) `{!Proper ((=) ==> iff) P}: ℘ A :=
+  Build_Sett _ _ P _.
+#[program]
+Definition SetEmpty {A: Type}: ℘ A := Build_Sett _ _ (fun _ => False) _.
+#[program]
+Definition SetFull {A: Type}: ℘ A := Build_Sett _ _ (fun _ => True) _.
+
+Notation "x ∈ P" := (SetContains x P) (at level 19).
+Notation "{{ x }}" := (SetSingleton x).
+Notation "{{ x ; y ; .. ; z }}" := (SetList (cons x (cons y (.. (cons z nil) ..)))).
+Notation "{{ x | P }}" := (SetProp (fun x => P)).
+Notation "{{ x : A | P }}" := (SetProp (fun x : A => P)).
+Notation "∅" := SetEmpty.
+
+Lemma set_contains_singleton {A: Type} `{Setoid A} (x: A):
+  forall u, u ∈ {{ x }} <-> u = x.
+Proof. reflexivity. Qed.
+
+Lemma set_contains_pair {A: Type} `{Setoid A} (x y: A):
+  forall u, u ∈ {{ x; y }} <-> u = x \/ u = y.
+Proof. unfold SetContains. simpl. firstorder. Qed.
+
+Lemma set_contains_empty {A: Type} `{Setoid A}:
+  forall (u: A), u ∈ ∅ -> False.
+Proof. contradiction. Qed.
+
+Lemma set_contains_full {A: Type} `{Setoid A}:
+  forall (u: A), u ∈ SetFull.
+Proof. unfold SetContains. simpl. auto. Qed.
+
+Section Powerset.
+
+  Context {X: Type} `{Setoid X}.
+
+  #[global]
+  Instance PowersetEquiv: Equiv (℘ X) :=
+    fun P Q => forall f, f ∈ P <-> f ∈ Q.
+
+  #[global]
+  Instance PowersetOrd: Ord (℘ X) :=
+    fun P Q => forall x, x ∈ P -> x ∈ Q.
+
+  #[program, global]
+  Instance PowersetSetoid: Setoid (℘ X).
+  Solve All Obligations with firstorder.
+
+  #[program, global]
+  Instance PowersetPoset: Poset (℘ X).
+  Solve All Obligations with firstorder.
+
+End Powerset.
+
+#[global]
+Add Parametric Morphism {A: Type} `{Equiv A}: (@SetContains A _)
+  with signature (=) ==> (=) ==> iff as SetContains_Morphism.
+Proof.
+  intros x y H__eq [f H__f] [g H__g] H__fg.
+  unfold SetContains. unfold SigEquiv in *. unfold equiv in *. unfold PowersetEquiv in *. simpl in *.
+  rewrite H__fg. rewrite H__eq. reflexivity.
+Qed.
+
+Class SgOp A `{Equiv A} := sg_op: A → A → A.
+Class SgSetOp A `{Equiv A} := sg_set_op: ℘ A → A.
+Class MonUnit A := mon_unit: A.
+
+#[global]
+Typeclasses Transparent SgOp SgSetOp MonUnit.
+
+Infix "&" := sg_op (at level 50, left associativity) : vsa.
+Notation "(&)" := sg_op (only parsing) : vsa.
+Notation "( x &)" := (sg_op x) (only parsing) : vsa.
+Notation "(& x )" := (fun y => y & x) (only parsing) : vsa.
+
+Definition UpperBound {A: Type} `{Poset A} (S: ℘ A) (u: A) := forall x, x ∈ S -> x ⊑ u.
+Definition LowerBound {A: Type} `{Poset A} (S: ℘ A) (u: A) := forall x, x ∈ S -> u ⊑ x.
--- Functions.v
+++ Functions.v
@@ -1,40 +1,51 @@
 From Coq Require Import Relations.Relations.
+From Coq Require Import Setoids.Setoid.
+From Coq Require Import Classes.Morphisms.
 From Coq Require Import Classes.RelationClasses.
 From VSA Require Import Basics.
 
 #[program]
-Definition image {X A: Type} `{Setoid X} `{Setoid A} (f : X -> A) (S : ℘ X) : ℘ A :=
-   {| set_prop := fun y => exists x, x ∈ S /\ y = f x |}.
+Definition Image {X A: Type} `{Equiv X} `{Setoid A} (f : X → A) (S : ℘ X) : ℘ A :=
+   Build_Sett _ _ (fun y => exists x, x ∈ S /\ y = f x) _.
 Next Obligation.
-  firstorder.
+  intros x y H__xy. split; intros [z [? H__f]]; exists z; firstorder.
 Qed.
 
-Class Increasing {A B: Type} (f: A -> B) `{Poset A} `{Poset B}: Prop :=
+#[global]
+Instance ImageProper {X A: Type} `{Setoid X} `{Setoid A}:
+  Proper ((=) ==> (=) ==> (=)) (@Image X A _ _ _).
+Proof.
+  intros f g H__fg P Q H__PQ x. split; intros [z []]; exists z; firstorder.
+Qed.
+
+Class Increasing {A B: Type} `{Poset A} `{Poset B} (f: A → B): Prop :=
   increasing : forall x y, x ⊑ y -> f x ⊑ f y.
 
-Class Injective {A B: Type} (f: A -> B) `{Equiv A} `{Equiv B}: Prop :=
+Class Injective {A B: Type} `{Equiv A} `{Equiv B} (f: A → B): Prop :=
   injective : forall x y, f x = f y -> x = y.
 
-Class Surjective {A B: Type} (f: A -> B) `{Equiv B}: Prop :=
+Class Surjective {A B: Type} `{Equiv A} `{Equiv B} (f: A → B): Prop :=
   surjective : forall y, exists x, y = f x.
 
-Class Bijective {A B: Type} (f: A -> B) `{Equiv A} `{Equiv B}: Prop :=
+Class Bijective {A B: Type} `{Equiv A} `{Equiv B} (f: A → B): Prop :=
   bijective : exists (g: B -> A), (forall x, g (f x) = x) /\ (forall y, f (g y) = y).
 
 #[export]
 Typeclasses Transparent Increasing Injective Surjective Bijective.
 
-Class PreserveSgOp {A B: Type} (f: A -> B) `{Equiv B} `(SgOp A) `(SgOp B): Prop :=
-  preserve_sg_op : forall x y, f (x & y) = f x & f y.
+Class PreserveSgOp {A B: Type} `{Equiv A} `{Equiv B} (f: A → B) `(!SgOp A) `(!SgOp B): Prop :=
+  preserve_sg_op : forall (x y: A), f (x & y) = f x & f y.
 
-Class PreserveSgSetOp {A B: Type} (f: A -> B) `{Equiv B} `(SgSetOp A) `(SgSetOp B): Prop :=
-  preserve_sg_set_op : forall (S: ℘ A), f (sg_set_op S) = sg_set_op (image f S).
+Class PreserveSgSetOp {A B: Type} `{Equiv A} `{Setoid B} (f: A → B) (SA: SgSetOp A) (SB: SgSetOp B): Prop :=
+  preserve_sg_set_op : forall (S: ℘ A), f (SA S) = SB (Image f S).
 
-Class StableSgOp {A : Type} (P: A -> Prop) `(SgOp A): Prop :=
+(* Here, we see it the first time we use the fact that a property (here P) can be seen as the *)
+(* set of elements which verify it*)
+Class StableSgOp {A : Type} `{Equiv A} (P: ℘ A) `(!SgOp A): Prop :=
   stable_sg_op : forall x y, P x -> P y -> P (x & y).
 
-Class StableSgSetOp {A: Type} (P: A -> Prop) `(SgSetOp A): Prop :=
-  stable_sg_set_op : forall (S: ℘ A), (forall x, x ∈ S -> P x) -> P (sg_set_op S).
+Class StableSgSetOp {A: Type} `{Equiv A} (P: ℘ A) (SA: SgSetOp A): Prop :=
+  stable_sg_set_op : forall (S: ℘ A), (forall x, x ∈ S -> P x) -> P (SA S).
 
 #[export]
 Typeclasses Transparent PreserveSgOp PreserveSgSetOp StableSgOp StableSgSetOp.
